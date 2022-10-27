d202d31c2cad7c81fbf2771ec5371fd7a97dcab6
ianiito <116592896+ianiito@users.noreply.github.com>
2022-10-26 20:50:09 -0700

    Update beer.sql

--- Beer Company/beer.sql
+++ Beer Company/beer.sql
@@ -5,5 +5,12 @@ CREATE DATABASE weeklybeer;
 USE weeklybeer;
 
 CREATE TABLE beer(
+  BeerId INT PRIMARY KEY AUTO INCREMENT ,
+  name VARCHAR(54) NOT NULL,
+  type VARCHAR(54) ,
+  price FLOAT(6,4) ,
+  number FLOAT(6,0),
+  totalP FLOAT(10,4)
+);
 
-)
+INSERT  INTO beer (name , type , price , number , totalP) VALUES(Ian,India Pale Ale,56.34,5,281.7);
